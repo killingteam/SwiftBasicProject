69d14a2df04bf54f2980c593981c43089dd743a8
cs <chenshuang@thejoyrun.com>
2019-07-04 10:39:56 +0800

    init project.

---
+++ SwiftBasicProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBasicProject
+//
+//  Created by cs on 2019/7/4.
+//  Copyright © 2019 NMSL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
