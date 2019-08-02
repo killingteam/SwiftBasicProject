25bea1ff331829d50cd9112f074d1f35fda23ad2
胡伟东 <duoyi@duoyis-macbook-air.local>
2019-08-02 11:47:50 +0800

    【Update】方法

--- SwiftBasicProject/WDSomeClass.swift
+++ SwiftBasicProject/WDSomeClass.swift
@@ -75,9 +75,43 @@ class StepCounter {
     }
 }
 
+struct PointTest {
+    var x = 0.0, y = 0.0
+    func isToTheRightOfX(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+
+enum TriStateSwitch {
+    case Off, Low, High
+    mutating func next() {
+        switch self {
+        case .Off:
+            self = .Low
+        case .Low:
+            self = .High
+        case .High:
+            self = .Off
+        }
+    }
+}
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+struct Animal {
+    let species: String
+    init?(species: String) {
+        if species.isEmpty { return nil }
+        self.species = species
+    }
+}
+
 class WDSomeClass: NSObject {
-    
-    
     let someResolution = Resolution()
     let someVideoMode = VideoMode()
     let vga = Resolution(width:640, height: 480)
@@ -130,6 +164,41 @@ class WDSomeClass: NSObject {
         stepCounter.totalSteps = 896
         // About to set totalSteps to 896
         // Added 536 steps
+        
+        let somePoint = PointTest(x: 4.0, y: 5.0)
+        if somePoint.isToTheRightOfX(x: 1.0) {
+            print("This point is to the right of the line where x == 1.0")
+        }
+        
+        var ovenLight = TriStateSwitch.Low
+        ovenLight.next()
+        // ovenLight 现在等于 .High
+        ovenLight.next()
+        // ovenLight 现在等于 .Off
+        
+        let threeTimesTable = TimesTable(multiplier: 3)
+        print("six times three is \(threeTimesTable[6])")
+        // 打印 "six times three is 18"
+        
+        let anonymousCreature = Animal(species: "")
+        // anonymousCreature 的类型是 Animal?, 而不是 Animal
+        
+        if anonymousCreature == nil {
+            print("The anonymous creature could not be initialized")
+        }
+        // 打印 "The anonymous creature could not be initialized"
+    }
+    
+    class Bank {
+        static var coinsInBank = 10_000
+        static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+            let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
+            coinsInBank -= numberOfCoinsToVend
+            return numberOfCoinsToVend
+        }
+        static func receive(coins: Int) {
+            coinsInBank += coins
+        }
     }
     
 }
